e67a2f7726ddb212730ae498ae919434809fc80d
devhyeon <devhyeon0312@gmail.com>
2021-04-30 22:28:54 +0900

    API DataClass 생성
    1. 필요한 Track Data
    2. 응답된 Response Data

---
+++ app/src/main/java/com/devhyeon/watchatask/network/itunes/data/ITunesResponse.kt
@@ -0,0 +1,13 @@
+package com.devhyeon.watchatask.network.itunes.data
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * @param(resultCount) : Long        : 결과 개수
+ * @param(results)     : List<Track> : 결과 데이터
+ * */
+
+data class ITunesResponse(
+    @SerializedName("resultCount")  val resultCount : Long,
+    @SerializedName("results")      val results     : List<ITunesTrack>
+)
---
+++ app/src/main/java/com/devhyeon/watchatask/network/itunes/data/ITunesTrack.kt
@@ -0,0 +1,23 @@
+package com.devhyeon.watchatask.network.itunes.data
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * @param(artistId)         : Long      : 아티스트의 아이디
+ * @param(collectionId)     : Long      : 앨범의 아이디
+ * @param(trackId)          : Long      : 트랙의 아이디
+ * @param(artistName)       : String    : 아티스트의 이름
+ * @param(collectionName)   : String    : 앨범의 이름
+ * @param(trackName)        : String    : 트랙의 이름
+ * @param(artworkUrl100)    : String    : 트랙에 이미지 URL
+ * */
+
+data class ITunesTrack(
+    @SerializedName("artistId")         val artistId        : Long,
+    @SerializedName("collectionId")     val collectionId    : Long,
+    @SerializedName("trackId")          val trackId         : Long,
+    @SerializedName("artistName")       val artistName      : String,
+    @SerializedName("collectionName")   val collectionName  : String,
+    @SerializedName("trackName")        val trackName       : String,
+    @SerializedName("artworkUrl100")    val artworkUrl100   : String
+)
