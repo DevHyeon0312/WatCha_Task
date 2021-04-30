4507231abe71b6010389b411ac888fad9df432d5
devhyeon <devhyeon0312@gmail.com>
2021-04-30 22:14:13 +0900

    NetworkModule.kt 작성
    1. okHttpClient 를 빌더패턴으로 생성하여 반환하는 메소드 작성
    2. Retrofit 을 빌더패턴으로 생성하여 반환하는 메소드 작성
    3. 의존성 주입

---
+++ app/src/main/java/com/devhyeon/watchatask/di/NetworkModule.kt
@@ -0,0 +1,36 @@
+package com.devhyeon.watchatask.di
+
+import com.devhyeon.watchatask.constant.HTTP_LOG_LEVEL
+import com.devhyeon.watchatask.constant.ITUNES_BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/** Network 관련 모듈 */
+val NetworkModule = module {
+
+    single { createOkHttpClient() }
+
+    single { createRetrofit(get(), ITUNES_BASE_URL) }
+
+}
+
+/** OkHttpClient 생성 */
+fun createOkHttpClient(): OkHttpClient {
+    val httpLoggingInterceptor = HttpLoggingInterceptor()
+    httpLoggingInterceptor.level = HTTP_LOG_LEVEL
+    return OkHttpClient.Builder()
+        .addInterceptor(httpLoggingInterceptor)
+        .build()
+}
+
+/** Retrofit 생성 */
+fun createRetrofit(okHttpClient: OkHttpClient, url: String): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl(url)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
