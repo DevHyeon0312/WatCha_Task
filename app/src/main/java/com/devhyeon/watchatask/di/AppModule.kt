bb3ada1a4fdcea8b80d5c0336ffeb05e032d55da
devhyeon <devhyeon0312@gmail.com>
2021-05-01 02:06:02 +0900

    AppModule.kt 수정
    1. MainViewModel 을 추가하여서 AppModule.kt 에 의존성 주입

--- app/src/main/java/com/devhyeon/watchatask/di/AppModule.kt
+++ app/src/main/java/com/devhyeon/watchatask/di/AppModule.kt
@@ -1,11 +1,15 @@
 package com.devhyeon.watchatask.di
 
 import com.devhyeon.watchatask.network.ITunesViewModel
+import com.devhyeon.watchatask.ui.activities.main.MainViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val AppModule = module {
     viewModel {
         ITunesViewModel(get())
+    }
+    viewModel {
+        MainViewModel()
     }
 }
