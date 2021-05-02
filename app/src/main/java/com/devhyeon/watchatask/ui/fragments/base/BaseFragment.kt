06864d6b977673896108f88fe0af0ef6e51bfcd2
devhyeon <devhyeon0312@gmail.com>
2021-05-02 19:47:51 +0900

    BaseFragment.kt 주석 추가

--- app/src/main/java/com/devhyeon/watchatask/ui/fragments/base/BaseFragment.kt
+++ app/src/main/java/com/devhyeon/watchatask/ui/fragments/base/BaseFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.devhyeon.watchatask.databinding.FragmentFavoritelistBinding
 
+/**
+ * Fragment Class 에서 상속받는 추상클래스
+ * */
 abstract class BaseFragment : Fragment() {
+    //viewBinding
     protected abstract fun initViewBinding(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
     protected abstract fun getViewRoot() : View
+    //데이터 등 기타 초기화
     protected abstract fun init()
+    //리스너, 옵저버
     protected abstract fun addListener()
     protected abstract fun addObserver()
 
