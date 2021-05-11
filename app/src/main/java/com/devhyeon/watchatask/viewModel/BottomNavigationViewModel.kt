c772b69e99b74cbce58d252ae97dac057779843e
devhyeon <devhyeon0312@gmail.com>
2021-05-12 02:08:16 +0900

    ViewModel 에서 Fragment 반환하는 코드를 기존코드로 원복.
    - ViewModel 이 Fragment 를 직접 반환하면서, Fragment 의 존재여부를 필요로 하는데, 즉, ViewModel 이 View 에 대해 알고 있어야 한다가 되어버린 것 같다.
    - 이는 ViewModel 이 View 를 반환하기 위해 해당 View 를 반드시 필요로 하는 결합상태가 되어버리고, 이는 좋지 않다고 판단하여 원복을 결정.
    - 위와 같은 경우보다는 차라리 View 가 data 를 가지고, 어떤 View 를 보여줄지 판단 후에 보여주는 것이 더 맞다고 생각. (이유 : dataBinding 에서도 true, false 를 가지고 visible 처리를 결정하는 것이 떠오름..)

--- app/src/main/java/com/devhyeon/watchatask/viewModel/BottomNavigationViewModel.kt
+++ app/src/main/java/com/devhyeon/watchatask/viewModel/BottomNavigationViewModel.kt
@@ -1,37 +1,32 @@
 package com.devhyeon.watchatask.viewModel
 
 import androidx.annotation.IdRes
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.devhyeon.watchatask.R
+import com.devhyeon.watchatask.constant.FRAGMENT_FAVORITE
 import com.devhyeon.watchatask.constant.FRAGMENT_SEARCH
-import com.devhyeon.watchatask.ui.fragments.FavoriteFragment
-import com.devhyeon.watchatask.ui.fragments.SearchFragment
 import kotlinx.coroutines.launch
 
 /**
- * BottomNavigation 의 선택된 data 에 따라 어떤 fragment 를 보여주어야 하는지 해당 fragment 반환
+ * BottomNavigation 에서 선택된 Item 을 LiveData 를 통해 관찰 할 수 있습니다.
+ *
  * */
-class BottomNavigationViewModel: ViewModel() {
-
-    private val _navigationData = MutableLiveData<Fragment>().apply { FRAGMENT_SEARCH }
-    val navigationData: LiveData<Fragment> get() = _navigationData
-
-    private val searchFragment by lazy { SearchFragment() }
-    private val favoriteFragment by lazy { FavoriteFragment() }
+class BottomNavigationViewModel: ViewModel(){
+    private val _navigationData = MutableLiveData<Int>().apply { FRAGMENT_SEARCH }
+    val navigationData: LiveData<Int> get() = _navigationData
 
     /** 클릭 이벤트에 따라 보여줘야하는 Fragment 선택 */
     fun clickNavigation(@IdRes id:Int) {
         viewModelScope.launch {
             when(id) {
                 R.id.menu_search -> {
-                    _navigationData.value = searchFragment
+                    _navigationData.value = FRAGMENT_SEARCH
                 }
                 R.id.menu_star -> {
-                    _navigationData.value = favoriteFragment
+                    _navigationData.value = FRAGMENT_FAVORITE
                 }
             }
         }
@@ -40,7 +35,7 @@ class BottomNavigationViewModel: ViewModel() {
     /** navigationData 의 값이 할당되기 전에 호출하는 경우 default 값으로 결정 */
     fun initNavigation() {
         if(navigationData.value == null) {
-            _navigationData.value = searchFragment
+            _navigationData.value = FRAGMENT_SEARCH
         }
     }
 }
