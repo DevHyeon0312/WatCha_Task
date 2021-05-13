7fec3c55be174c7e23b0fc095006e3b910bcae64
devhyeon <devhyeon0312@gmail.com>
2021-05-13 14:06:02 +0900

    BaseFragment.kt -> BaseBindingFragment.kt 로 변경
    - addListener() 와, addObserver() 가 과연 이후 BaseFragment 를 상속받을때, 모든 Fragment 에 필요한가? 에서 아니다라는 결론
    - 따라서 수정

--- app/src/main/java/com/devhyeon/watchatask/ui/fragments/base/BaseBindingFragment.kt
+++ app/src/main/java/com/devhyeon/watchatask/ui/fragments/base/BaseBindingFragment.kt
@@ -9,15 +9,12 @@ import androidx.fragment.app.Fragment
 /**
  * Fragment Class 에서 상속받는 추상클래스
  * */
-abstract class BaseFragment : Fragment() {
+abstract class BaseBindingFragment : Fragment() {
     //viewBinding
     protected abstract fun initViewBinding(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
     protected abstract fun getViewRoot() : View
     //데이터 등 기타 초기화
     protected abstract fun init()
-    //리스너, 옵저버
-    protected abstract fun addListener()
-    protected abstract fun addObserver()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,11 +24,5 @@ abstract class BaseFragment : Fragment() {
         initViewBinding(inflater, container, savedInstanceState)
         init()
         return getViewRoot()
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        addListener()
-        addObserver()
     }
 }
