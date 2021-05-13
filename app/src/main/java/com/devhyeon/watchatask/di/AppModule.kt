c146dd4c70efd2223b9556ba6d68e15b52bd968e
devhyeon <devhyeon0312@gmail.com>
2021-05-13 14:11:30 +0900

    AppModule.kt 수정
    - 제거된 viewModel 삭제
    - 추가된 viewModel 삽입

--- app/src/main/java/com/devhyeon/watchatask/di/AppModule.kt
+++ app/src/main/java/com/devhyeon/watchatask/di/AppModule.kt
@@ -2,25 +2,19 @@ package com.devhyeon.watchatask.di
 
 import android.content.Context
 import com.devhyeon.watchatask.db.favorite.FavoriteDatabase
-import com.devhyeon.watchatask.db.FavoriteViewModel
-import com.devhyeon.watchatask.network.ITunesViewModel
 import com.devhyeon.watchatask.viewModel.BottomNavigationViewModel
+import com.devhyeon.watchatask.viewModel.TrackListViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val AppModule = module {
-    viewModel {
-        ITunesViewModel(get())
-    }
     viewModel {
         BottomNavigationViewModel()
     }
 
     single { createDataBase(get()) }
 
-    viewModel {
-        FavoriteViewModel(get())
-    }
+    single { TrackListViewModel(get(), get()) }
 }
 
 fun createDataBase(context: Context) : FavoriteDatabase? {
